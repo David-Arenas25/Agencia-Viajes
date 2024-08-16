3593d031d356e1671378260aa39d65fcf3bccd7c
David-Arenas25 <darozco25@gmail.com>
2024-08-16 11:45:24 -0500

    Update UsuarioController.js

--- controller/UsuarioController.js
+++ controller/UsuarioController.js
@@ -1,4 +1,4 @@
-import { usuarios } from "../model/UsuarioModel.js";
+import { usuarios } from "../Model/UsuarioModel.js";
 export function filtrarPorUsuarios() {
 
   document.getElementById('filter-users').innerHTML = '';
